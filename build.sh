e5d86c8e4497714feffb42ff5e19955013859007
Jürgen Eckel <juergen@riddleandcode.com>
2021-09-03 05:12:40 +0200

    * simplified requirements.txt to have collusion and friction with the RTD environment

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -x
 rm -rf venv
 python3.7 -mvirtualenv ./venv
+# the RTD specific environment preparation
 ./venv/bin/python -m pip install --upgrade --no-cache-dir pip setuptools
 ./venv/bin/python -m pip install --upgrade --no-cache-dir mock==1.0.1 pillow==5.4.1 'alabaster>=0.7,<0.8,!=0.7.5' commonmark==0.8.1 recommonmark==0.5.0 mkdocs
+# the R&C specific packages
 ./venv/bin/python -m pip install --exists-action=w --no-cache-dir -r requirements.txt
